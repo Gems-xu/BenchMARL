8ed6f23813e3b7dc0673e715b5179c2abdd1df29
Gems <87588116+Gems-xu@users.noreply.github.com>
2025-10-31 10:49:46 +0800

    Add uv management

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello from Gems' BenchMARL!")
+    print("python -m benchmarl.run algorithm=mappo task=vmas/navigation experiment.train_device='cuda'")
+
+
+if __name__ == "__main__":
+    main()
