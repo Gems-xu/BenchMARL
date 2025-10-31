39e50166c0c60759e3ace62c4d0c0de9f5e53cef
Gems <87588116+Gems-xu@users.noreply.github.com>
2025-10-31 11:47:58 +0800

    修正主函数中的命令行示例，添加前缀“uv”以确保正确执行

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     print("Hello from Gems' BenchMARL!")
-    print("python -m benchmarl.run algorithm=mappo task=vmas/navigation experiment.train_device='cuda'")
+    print("uv run python -m benchmarl.run algorithm=mappo task=vmas/navigation experiment.train_device='cuda' experiment.render=False")
+    # experiment.max_n_frames=1_000_000
+    # 或者使用迭代次数: experiment.max_n_iters=500
 
 
 if __name__ == "__main__":
